75b0486c4c423260761c72652252c2e3bab0c9fd
Anton Kokorin <ant.kkrn@gmail.com>
2024-06-02 21:08:21 +0700

    add main loader

---
+++ bot/loader.py
@@ -0,0 +1,29 @@
+from pyrogram import Client
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from config_data.config import load_config
+from database import Database
+
+config = load_config(path=None)
+
+db = Database(
+    name=config.db.postgres_db,
+    user=config.db.postgres_user,
+    password=config.db.postgres_password,
+    host=config.db.db_host,
+    port=config.db.db_port,
+)
+database_url = f"postgresql+asyncpg://{db.user}:{db.password}@{db.host}:{db.port}/{db.name}"
+engine = create_async_engine(database_url, echo=False)
+async_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+sentry_url = config.sentry_url.url
+
+client = Client(
+    config.user_bot.app_name,
+    api_id=config.user_bot.api_id,
+    api_hash=config.user_bot.api_hash,
+)
