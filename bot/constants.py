61ce6331a14fd128cea7cbb8136665424202e59a
Anton Kokorin <ant.kkrn@gmail.com>
2024-06-03 12:45:09 +0700

    change time delay to real value

--- bot/constants.py
+++ bot/constants.py
@@ -1,7 +1,7 @@
 FIRST_MSG = "Текст1"
 SECOND_MSG = "Текст2"
 THIRD_MSG = "Текст3"
-FIRST_DELAY = 1 * 5  # 6 * 60
-SECOND_DELAY = 3 * 5  # 39 * 60
-THIRD_DELAY = 4 * 5  # 26 * 60 * 60
+FIRST_DELAY = 6 * 60
+SECOND_DELAY = 39 * 60
+THIRD_DELAY = 26 * 60 * 60
 TRIGGER_WORDS = ["прекрасно", "ожидать"]
